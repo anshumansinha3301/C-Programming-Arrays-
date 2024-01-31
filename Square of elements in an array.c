169f0ec2516e52157cb7f09420e9de6513f1768d
Anshuman Sinha <138311612+anshumansinha3301@users.noreply.github.com>
2024-01-31 17:21:22 +0530

    Square of elements in an array.c

---
+++ Square of elements in an array.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() {
+    int size;
+
+    printf("Enter the size of the array: ");
+    scanf("%d", &size);
+
+    int arr[size];
+
+  
+    printf("Enter %d elements:\n", size);
+    for (int i = 0; i < size; i++) {
+        printf("Element %d: ", i + 1);
+        scanf("%d", &arr[i]);
+    }
+
+
+    printf("Square of array elements:\n");
+    for (int i = 0; i < size; i++) {
+        printf("%d^2 = %d\n", arr[i], arr[i] * arr[i]);
+    }
+
+    return 0;
+}
