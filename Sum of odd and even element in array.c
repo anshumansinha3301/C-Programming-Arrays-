3251c795787c839516880a622654c3180a5e155e
Anshuman Sinha <138311612+anshumansinha3301@users.noreply.github.com>
2024-01-31 17:20:02 +0530

    Sum of odd and even element in array.c

---
+++ Sum of odd and even element in array.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main() {
+    int size, sumEven = 0, sumOdd = 0;
+    printf("Enter the size of the array: ");
+    scanf("%d", &size);
+
+    
+    int arr[size];
+
+    
+    printf("Enter %d elements:\n", size);
+    for (int i = 0; i < size; i++) {
+        printf("Element %d: ", i + 1);
+        scanf("%d", &arr[i]);
+        if (arr[i] % 2 == 0) {
+            sumEven += arr[i];
+        } else {
+            sumOdd += arr[i];
+        }
+    }
+
+    
+    printf("Sum of even elements: %d\n", sumEven);
+    printf("Sum of odd elements: %d\n", sumOdd);
+
+    return 0;
+}
