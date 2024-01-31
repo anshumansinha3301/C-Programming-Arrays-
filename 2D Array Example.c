c12d4d27b1df89b2a492556614d2a05fe6c10fe8
Anshuman Sinha <138311612+anshumansinha3301@users.noreply.github.com>
2024-01-31 18:32:24 +0530

    Create 2D Array Example.c

---
+++ 2D Array Example.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+#define ROWS 3  
+#define COLS 4  
+int main() {
+    int array[ROWS][COLS];
+    int i, j;
+
+   
+    printf("Enter elements for the 2D array (%d x %d):\n", ROWS, COLS);
+    for (i = 0; i < ROWS; i++) {
+        for (j = 0; j < COLS; j++) {
+            printf("Enter element at position [%d][%d]: ", i, j);
+            scanf("%d", &array[i][j]);
+        }
+    }
+
+  
+    printf("\nEntered 2D array is:\n");
+    for (i = 0; i < ROWS; i++) {
+        for (j = 0; j < COLS; j++) {
+            printf("%d\t", array[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
