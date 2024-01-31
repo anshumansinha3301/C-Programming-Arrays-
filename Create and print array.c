2d719aa6dc4cd8e04977b66983471bb7bd460c5f
Anshuman Sinha <138311612+anshumansinha3301@users.noreply.github.com>
2024-01-31 17:16:10 +0530

    Create and print array.c

---
+++ Create and print array.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+    int size;
+    printf("Enter the size of the array: ");
+    scanf("%d", &size);
+
+    int arr[size];
+    printf("Enter %d elements:\n", size);
+    for (int i = 0; i < size; i++) {
+        printf("Element %d: ", i + 1);
+        scanf("%d", &arr[i]);
+    }
+    printf("Array elements are: ");
+    for (int i = 0; i < size; i++) {
+        printf("%d ", arr[i]);
+    }
+    return 0;
+}
