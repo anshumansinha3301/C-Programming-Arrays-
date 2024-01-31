30e32fca6666664d00f308ec7432f7300b34f840
Anshuman Sinha <138311612+anshumansinha3301@users.noreply.github.com>
2024-01-31 17:23:05 +0530

    Display array elements which are prime number.c

---
+++ Display array elements which are prime number.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+
+bool isPrime(int num) {
+    if (num <= 1) {
+        return false;
+    }
+    for (int i = 2; i * i <= num; i++) {
+        if (num % i == 0) {
+            return false;
+        }
+    }
+    return true;
+}
+
+int main() {
+    int size;
+
+    
+    printf("Enter the size of the array: ");
+    scanf("%d", &size);
+
+    
+    int arr[size];
+
+
+    printf("Enter %d elements:\n", size);
+    for (int i = 0; i < size; i++) {
+        printf("Element %d: ", i + 1);
+        scanf("%d", &arr[i]);
+    }
+
+
+    printf("Prime elements from the array:\n");
+    for (int i = 0; i < size; i++) {
+        if (isPrime(arr[i])) {
+            printf("%d\n", arr[i]);
+        }
+    }
+
+    return 0;
+}
