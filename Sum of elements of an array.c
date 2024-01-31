9c5a5aa24eca29b2a94472f921f25c26bd347494
Anshuman Sinha <138311612+anshumansinha3301@users.noreply.github.com>
2024-01-31 17:18:03 +0530

    Sum of elements of an array.c

---
+++ Sum of elements of an array.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    int size, sum = 0;
+    printf("Enter the size of the array: ");
+    scanf("%d", &size);
+
+    int arr[size];
+
+    printf("Enter %d elements:\n", size);
+    for (int i = 0; i < size; i++) {
+        printf("Element %d: ", i + 1);
+        scanf("%d", &arr[i]);
+
+        sum += arr[i];
+    }
+    printf("Sum of array elements: %d\n", sum);
+
+    return 0;
+}
